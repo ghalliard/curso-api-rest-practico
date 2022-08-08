f0c6d77bb40e943ad536992725f31785a9de6d81
ghalliard <gabrielmontoyahuamani26@gmail.com>
2022-08-07 22:18:10 -0500

    Primera seccion mejorada, faltan animaciones

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const get_trending_movies_preview = async() =>{
 
     const movies_data = data.results;
     console.log(movies_data);
-
+    /*
     movies_data.forEach(movie => {
         const movie_list = document.querySelector('.trending-preview--movie-list');
         const movie_container = document.createElement('div');
@@ -26,5 +26,27 @@ const get_trending_movies_preview = async() =>{
         movie_description.appendChild(movie_title);
         movie_container.appendChild(movie_description);
     });
+    */
+    for(let i = 0; i < 5; i++){
+        const article = document.querySelector('.movie-list');
+        const movie_poster_container = document.createElement('div');
+
+        const movie_img = document.createElement('img');
+        movie_img.setAttribute('src', `https://image.tmdb.org/t/p/w300${movies_data[i].poster_path}`);
+        const movie_description = document.createElement('div');
+        movie_description.classList.add('movie-description');
+        const movie_title = document.createElement('h1');
+        movie_title.innerText = movies_data[i].title
+        const main_button = document.createElement('button');
+        main_button.classList.add('main-button');
+        main_button.innerText = 'COMPRAR';
+
+        movie_description.appendChild(movie_title);
+        movie_description.appendChild(main_button);
+        movie_poster_container.appendChild(movie_img);
+        movie_poster_container.appendChild(movie_description);
+        article.appendChild(movie_poster_container)
+    }
+    
 }
 get_trending_movies_preview();
