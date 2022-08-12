7f02056ae02d47ab903861e34b812f2d3e5bfc76
ghalliard <gabrielmontoyahuamani26@gmail.com>
2022-08-11 23:41:18 -0500

    footer semi-terminado. falta perfeccionar unas cosas

--- src/main.js
+++ src/main.js
@@ -5,28 +5,7 @@ const get_trending_movies_preview = async() =>{
 
     const movies_data = data.results;
     console.log(movies_data);
-    /*
-    movies_data.forEach(movie => {
-        const movie_list = document.querySelector('.trending-preview--movie-list');
-        const movie_container = document.createElement('div');
-        movie_container.classList.add('movie-container');
-        const movie_image = document.createElement('img');
-        movie_image.classList.add('movie-img');
-        movie_image.setAttribute('alt', movie.original_title);
-        movie_image.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`);
-
-        movie_container.appendChild(movie_image);
-        movie_list.appendChild(movie_container);
-
-        const movie_description = document.createElement('div');
-        movie_description.classList.add('movie-description');
-        const movie_title = document.createElement('h2');
-        movie_title.innerText = movie.title;
-
-        movie_description.appendChild(movie_title);
-        movie_container.appendChild(movie_description);
-    });
-    */
+    
     for(let i = 0; i < 5; i++){
         const article = document.querySelector('.movie-list');
         const movie_poster_container = document.createElement('div');
@@ -47,6 +26,14 @@ const get_trending_movies_preview = async() =>{
         movie_poster_container.appendChild(movie_description);
         article.appendChild(movie_poster_container)
     }
+
     
+    for(let i = 5; i < 10; i++){
+        const div = document.querySelector(`.movie${i-4}`);
+        const img = document.createElement('img');
+        img.setAttribute('src', `https://image.tmdb.org/t/p/w300${movies_data[i].poster_path}`);
+
+        div.appendChild(img);
+    }
 }
 get_trending_movies_preview();
