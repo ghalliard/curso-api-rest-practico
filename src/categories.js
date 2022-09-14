456c0f7f795aee67b4f37b6e6db9c07bfae42a2a
ghalliard <gabrielmontoyahuamani26@gmail.com>
2022-09-14 07:51:45 -0500

    listado semi ok

--- src/categories.js
+++ src/categories.js
@@ -1,8 +1,21 @@
+const list_categorie_container = document.querySelector('.categorie-list-div--open');
+const categorie_list_div_close = document.querySelector('.categorie-list-div--close');
+const categorie_button_container = document.querySelector('.close-categories');
+
 const display_categories = document.querySelector('.display-categories');
 display_categories.addEventListener('click', () => {
-    const list_categorie_container = document.querySelector('.categorie-list-div--open');
+    
     list_categorie_container.setAttribute('id', 'open');
-    const categorie_list_div_close = document.querySelector('.categorie-list-div--close');
+
+    
     categorie_list_div_close.setAttribute('id', 'close');
+
+    categorie_button_container.setAttribute('id', 'open')
 });
 
+const close_categories = document.getElementById('close-category-button');
+close_categories.addEventListener('click', () => {
+    list_categorie_container.removeAttribute('id');
+    categorie_list_div_close.removeAttribute('id');
+    categorie_button_container.removeAttribute('id');
+});
