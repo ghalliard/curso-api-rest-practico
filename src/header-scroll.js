e349e82e5361e3b4e06ca3cc9a08f92b47c9b1cf
unknown <Sistemas@fu73243540-ext.financieraoh.local>
2022-09-20 17:14:27 -0500

    algunas mejoras en el header

--- src/header-scroll.js
+++ src/header-scroll.js
@@ -1,4 +1,11 @@
 window.addEventListener("scroll", () => {
     let header = document.querySelector('.main-header');
+    let navbar_icon_menu = document.querySelector('.navbar-icon-menu div');
+    let navbar_icon_profile = document.querySelector('.navbar-icon-profile div');
+    let navbar_icon_logo = document.querySelector('.navbar-icon-logo div')
+
+    navbar_icon_menu.classList.toggle('scroll-box-style', window.scrollY > 640);
+    navbar_icon_profile.classList.toggle('scroll-box-style', window.scrollY > 640);
+    navbar_icon_logo.classList.toggle('scroll-box-style', window.scrollY > 640);
     header.classList.toggle('scroll-down', window.scrollY > 640);
 });
